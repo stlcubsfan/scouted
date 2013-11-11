079d7d92cbc654282758157c507ec5cbdbd48abf
Brian Coles <stlcubsfan@gmail.com>
2013-11-11 15:22:57 -0600

    still working on font-awesome

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module Scouted
       g.view_specs false
       g.helper_specs false
     end
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
